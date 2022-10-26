41c5b27e881b50dd22cd53d379b668ddd4b1a0b6
McHerman <karl@krauses.dk>
2022-10-26 21:00:25 +0800

    Added dataset

--- ML.py
+++ ML.py
@@ -3,7 +3,7 @@ from numpy import loadtxt
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 # load the dataset
-dataset = loadtxt('pima-indians-diabetes.csv', delimiter=',')
+dataset = loadtxt('pulsar_data_train.csv', delimiter=',')
 # split into input (X) and output (y) variables
 X = dataset[:,0:8]
 y = dataset[:,8]
