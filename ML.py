2b2c929c83850f911e935381a580b863890acb76
Karl Herman Krause <66793169+McHerman@users.noreply.github.com>
2022-11-21 15:26:47 +0800

    Update ML.py

--- ML.py
+++ ML.py
@@ -1,13 +1,17 @@
-# first neural network with keras tutorial
 from numpy import loadtxt
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 # load the dataset
-dataset = loadtxt('pulsar_data_train.csv', delimiter=',')
-dataset_delete = numpy.delete(dataset,0,axis=row)
+dataset = loadtxt('pulsar_data_training.csv', delimiter=',',skiprows=1)
 # split into input (X) and output (y) variables
-X = dataset_delete[:,0:9]
-y = dataset_delete[:,9]
+X = dataset[:,0:8]
+Y = dataset[:,8]
+
+validation = loadtxt('pulsar_data_test.csv', delimiter=',',skiprows=1)
+
+vX = validation[:,0:8]
+vY = validation[:,8]
+
 # define the keras model
 model = Sequential()
 model.add(Dense(12, input_shape=(8,), activation='relu'))
@@ -16,7 +20,8 @@ model.add(Dense(1, activation='sigmoid'))
 # compile the keras model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 # fit the keras model on the dataset
-model.fit(X, y, epochs=150, batch_size=10)
+history = model.fit(X, Y, validation_split = 0.33, epochs=150, batch_size=10)
 # evaluate the keras model
-_, accuracy = model.evaluate(X, y)
+_, accuracy = model.evaluate(vX, vY)
 print('Accuracy: %.2f' % (accuracy*100))
+print(history)
