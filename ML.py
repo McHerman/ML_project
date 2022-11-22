75ff4dce5b2ee7b13060bd2c617f3b981e3bd884
Karl Herman Krause <66793169+McHerman@users.noreply.github.com>
2022-11-22 13:51:27 +0800

    Works

--- ML.py
+++ ML.py
@@ -1,16 +1,19 @@
-from numpy import loadtxt
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+import matplotlib.pyplot as plt
+#from numpy import loadtxt
+import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 # load the dataset
-dataset = loadtxt('pulsar_data_training.csv', delimiter=',',skiprows=1)
-# split into input (X) and output (y) variables
-X = dataset[:,0:8]
-Y = dataset[:,8]
+dataset = np.loadtxt('pulsar_data.csv', delimiter=',',skiprows=1)
+# split into input (X) and outpu½t (y) variables
+X = dataset[:13423,0:8]
+Y = dataset[:13423,8]
 
-validation = loadtxt('pulsar_data_test.csv', delimiter=',',skiprows=1)
+vX = dataset[13424:,0:8]
+vY = dataset[13424:,8] 
 
-vX = validation[:,0:8]
-vY = validation[:,8]
 
 # define the keras model
 model = Sequential()
@@ -20,8 +23,19 @@ model.add(Dense(1, activation='sigmoid'))
 # compile the keras model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 # fit the keras model on the dataset
-history = model.fit(X, Y, validation_split = 0.33, epochs=150, batch_size=10)
+history = model.fit(X, Y, epochs=20, batch_size=100)
+weights = model.get_weights()
 # evaluate the keras model
 _, accuracy = model.evaluate(vX, vY)
 print('Accuracy: %.2f' % (accuracy*100))
-print(history)
+#print(weights)
+
+print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['accuracy'])
+#plt.plot(history.history['val_accuracy'])
+#plt.plot(history.history['loss'])
+#plt.plot(history.history['val_loss'])
+#plt.plot(weights)
+
+plt.show()
